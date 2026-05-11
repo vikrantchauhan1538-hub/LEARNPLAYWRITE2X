30000915aa329753ea602dbbdc0daaabed823881
vikrantchauhan1538-hub <vikrant.chauhan1538@gmail.com>
2026-05-11 16:24:00 +0530

    Reorganize Chapter 1: delete old Ch_1_Basics.js and add chapter_01_Basics directory with structured lesson files

--- Ch_1_Basics.js
+++
@@ -1,9 +0,0 @@
-console.log('Hi vikrant');
-let s = 4;
-let d = 4;
-let r = s + d;
-console.log(r);
-console.log(++s);
-console.log(process.arch);
-
-console.log(process.platform);
---
+++ chapter_01_Basics/01_Basics.js
@@ -0,0 +1,5 @@
+console.log("Hi this is first test case");
+ var a = 4;
+ let b = 5;
+ const amit = "Ramesh";
+ console.log(a,b,amit);
