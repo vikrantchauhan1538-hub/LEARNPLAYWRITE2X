a184df6d7cde8f48a48fe2c38882a15955f0da1d
vikrantchauhan1538-hub <vikrant.chauhan1538@gmail.com>
2026-05-11 22:34:33 +0530

    Update chapter 1 basics files with new examples and content

--- chapter_01_Basics/01_Basics.js
+++ chapter_01_Basics/01_Basics.js
@@ -1,8 +1,8 @@
 console.log("Hi this is first test case");
- var a = 4;
- console.log(a)
- let b = 5;
- const amit = "Ramesh";
- var a =15;
- a = 78;
- console.log(a,b,amit)
+var a = 4;
+console.log(a)
+let b = 5;
+const amit = "Ramesh";
+var a = 15;
+a = 78;
+console.log(a, b, amit)
